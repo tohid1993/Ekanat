660aa2a57367c52309caee359b4020fd088e366d
tohid karbasi <tohid.k1993@gmail.com>
2022-06-25 00:08:32 +0430

    1401.04.04

--- Site/EKanatSite/src/app/fields/add-field/add-field.component.ts
+++ Site/EKanatSite/src/app/fields/add-field/add-field.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import * as Leaflet from 'leaflet';
 import "leaflet-draw";
 // import "leaflet.gridlayer.googlemutant";
-import * as GeoSearch from 'leaflet-geosearch'
+import * as GeoSearch from 'leaflet-geosearch';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-add-field',
@@ -11,6 +12,14 @@ import * as GeoSearch from 'leaflet-geosearch'
 })
 export class AddFieldComponent implements OnInit {
   map: Leaflet.Map|undefined;
+
+  minHA:number = 1;
+  maxHA:number = 50;
+
+  FieldCordinates:any;
+  FieldArea:number = 0;
+
+  currentStep:number = 1;
 
   constructor() { }
 
@@ -22,9 +31,13 @@ export class AddFieldComponent implements OnInit {
         attribution: 'EKANAT.COM ❤️'
     }).addTo(this.map);
 
+
+
+
     let drawnItems = Leaflet.featureGroup().addTo(this.map);
 
-    this.map .addControl(new Leaflet.Control.Draw({
+
+    var drawControlFull = new Leaflet.Control.Draw({
       edit: {
         featureGroup:drawnItems,
         remove:true,
@@ -41,24 +54,70 @@ export class AddFieldComponent implements OnInit {
             showArea: true
         }
       }
-    }));
+    });
+  
+    var drawControlRemoveOnly = new Leaflet.Control.Draw({
+      edit: {
+        featureGroup:drawnItems,
+        remove:true,
+        edit:false
+      },
+      draw: {
+        circle:false,
+        circlemarker:false,
+        marker:false,
+        polyline:false,
+        rectangle:false,
+        polygon: false
+      }
+    });
 
+
+    this.map.addControl(drawControlFull);
+
+    let selfMap = this.map;
+    let self = this;
 
     this.map.on(Leaflet.Draw.Event.CREATED, function (event:any) {
       var type = event.layerType,
       layer = event.layer;
+
+
       if (type === 'polygon') {
         var area = Leaflet.GeometryUtil.geodesicArea(layer.getLatLngs()[0]);
-        area = Math.round(((area/1000) + Number.EPSILON) * 100) / 100
-        layer.bindTooltip(area + " هکتار " , {direction:'rtl' , permanent:true}).openTooltip();
-        drawnItems.addLayer(layer);
-      }  
-      console.log(
-        layer.getLatLngs()
-      );
-      
-    });
+        area = Math.round(((area/10000) + Number.EPSILON) * 100) / 100
 
+        if(area>=self.minHA && area<=self.maxHA){
+          layer.bindTooltip(area + " هکتار " , {direction:'rtl' , permanent:true}).openTooltip();
+          drawnItems.addLayer(layer);
+  
+          
+          self.FieldCordinates = layer.getLatLngs();
+          self.FieldArea = area;
+  
+          drawControlFull.remove();
+          drawControlRemoveOnly.addTo(selfMap);
+        }else{
+          Swal.fire({
+            icon:"warning",
+            title:"زمین انتخابی نا معتبر است",
+            text:"اندازه زمین انتخابی باید بیشتر از "+self.minHA+" و کمتر از " + self.maxHA + " هکتار باشد.",
+            confirmButtonText:"متوجه شدم"
+          })
+        }
+      }  
+    })
+
+
+
+    this.map.on(Leaflet.Draw.Event.DELETED, function(e) {
+      self.FieldCordinates = undefined;
+      self.FieldArea = 0;
+
+      drawControlRemoveOnly.remove();
+      drawControlFull.addTo(selfMap);
+    })
   }
+  
 
 }
