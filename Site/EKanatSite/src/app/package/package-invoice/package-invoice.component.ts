001c4f3e92b7d2f134b4597af4385c93b14c9a62
tohid karbasi <tohid.k1993@gmail.com>
2022-09-21 23:44:23 +0430

    1401.06.30

--- Site/EKanatSite/src/app/package/package-invoice/package-invoice.component.ts
+++ Site/EKanatSite/src/app/package/package-invoice/package-invoice.component.ts
@@ -17,6 +17,8 @@ export class PackageInvoiceComponent implements OnInit {
   isChanged:boolean = false;
   FieldsList:FieldsListVM[] = [];
 
+  PeriodCount:number=1;
+
   
   constructor(
     config: NgbModalConfig, 
